a595f470a9a8bfa82f1c602eae20c478bc5379dc
szabogtamas <szabogtamas@gmail.com>
2021-05-22 12:17:05 +0200

    Fixed file listing

--- scripts/sync_folder.R
+++ scripts/sync_folder.R
@@ -15,7 +15,7 @@ library("googledrive")
 
 drive_auth(cache = "/home/rstudio/local_files/.secrets", use_oob = TRUE)
 
-already_uploaded_files <- drive_find(pattern = file.path("~", DRIVE_FOLDER))
+already_uploaded_files <- drive_ls(file.path("~", DRIVE_FOLDER))
 locally_present_files <- dir(LOCAL_FOLDER, full.names=TRUE)
 
 for(local_file in locally_present_files){
