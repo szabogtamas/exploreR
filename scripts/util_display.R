f68cde7f2e76f04067d76c9dcc6b98a1c1e9acef
szabogtamas <szabogtamas@gmail.com>
2021-05-29 22:51:39 +0200

    Added pheatmap patch

--- scripts/util_display.R
+++ scripts/util_display.R
@@ -14,3 +14,13 @@ show_tab <- function(tab){
     )
   )
 }
+
+### Patching Pheatmap with diagonal column labels;
+### Idea from https://www.thetopsites.net/article/54919955.shtml
+draw_colnames_30 <- function (coln, gaps, ...) {
+    coord = pheatmap:::find_coordinates(length(coln), gaps)
+    x = coord$coord - 0.5 * coord$size
+    res = grid:::textGrob(coln, x = x, y = unit(1, "npc") - unit(3,"bigpts"), vjust = 0.5, hjust = 1, rot = 30, gp = grid:::gpar(...))
+    return(res)}
+
+assignInNamespace(x="draw_colnames", value="draw_colnames_30", ns=asNamespace("pheatmap"))
