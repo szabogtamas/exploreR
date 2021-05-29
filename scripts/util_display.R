fb43b4bb867a1eb017c878e764360ae98e9b2bdf
szabogtamas <szabogtamas@gmail.com>
2021-05-29 22:45:22 +0200

    Added function showing a pretty table

--- scripts/util_display.R
+++ scripts/util_display.R
@@ -1,1 +1,16 @@
-show_tab <- function()
+show_tab <- function(tab){
+  datatable(
+    tab, extensions = "Buttons",
+    options = list(
+      scrollX="400px",
+      dom = "Bfrtip",
+      buttons = list(
+        list(
+          extend = "collection",
+          buttons = c("csv", "excel"),
+          text = "Download"
+        )
+      )
+    )
+  )
+}
