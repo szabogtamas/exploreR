b1911292eed6345adfe3eda4fe89f79bbe7916d5
szabogtamas <szabogtamas@gmail.com>
2021-05-29 22:48:09 +0200

    Added function that pulls from data depo

--- scripts/util_dataset.R
+++ scripts/util_dataset.R
@@ -1,1 +1,16 @@
-
+read_from_drive <- function(drive_path) {
+  path <- drive_get(drive_path)
+  drive_download(path, overwrite = TRUE)
+  data_file <- basename(drive_path)
+  data_table <- data_file %>%
+    gzfile() %>%
+    read.csv(sep="|", stringsAsFactors=FALSE, header=FALSE)
+  colnames(data_table) <- c(
+    "db_row", "patient_id", "gender", "age", "order_id", "date", "time", "param",
+    "value_c", "value_n", "is_numeric", "needs_revision", "qc_comment", "instrument", "unit",
+    "normal_range", "ward_id", "lab_comment"
+  )
+  data_table$file_name <- data_file
+  unlink(data_file)
+  return(data_table)
+}
